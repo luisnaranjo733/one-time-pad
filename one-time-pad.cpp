e8c2daed6910f690c47cc50b715660e84820c035
Luis Naranjo <jonaranj@microsoft.com>
2021-08-29 19:29:06 -0700

    project skeleton

---
+++ one-time-pad.cpp
@@ -0,0 +1,12 @@
+﻿// one-time-pad.cpp : Defines the entry point for the application.
+//
+
+#include "one-time-pad.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
